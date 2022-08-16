f090dcab6f297e58482f8cc42299d20463ed7b44
davidquindoza <dmquindoza@gmail.com>
2022-08-16 17:12:03 +0800

    placed data on dashboard

--- src/app/adminComponents/adminpage/adminpage.component.ts
+++ src/app/adminComponents/adminpage/adminpage.component.ts
@@ -9,14 +9,32 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class AdminpageComponent implements OnInit {
   ticketArray: any;
+  totalTickets!: number;
+  pendingInquiry!: any;
+  currentlyServiced!: any;
+  carRelease!: any;
+
+  userArray:any;
+  totalEmployees!: any;
 
   constructor(private afs: AngularFirestore, public authService: AuthService) {
     this.afs.collection<any>('tickets').valueChanges().subscribe(data => {
       // Ticket object array
       this.ticketArray = data;
+      this.totalTickets = this.ticketArray.length;
+      this.pendingInquiry = this.ticketArray.filter((x)=> x.status === "Pending Inquiry");
+      this.currentlyServiced = this.ticketArray.filter((x)=> x.status === "Undergoing Repair/Maintenance");
+      this.carRelease = this.ticketArray.filter((x)=> x.status === "For Release");
 
       // Perform data queries here
       
+    })
+    this.afs.collection<any>('users').valueChanges().subscribe(data => {
+      this.userArray=data;
+      this.totalEmployees = this.userArray.filter((x)=> x.permission === 1);
+
+
+
     })
   }
 
